9b2597d139354c98ed0b24566c8431bc175fb4b4
vickyvk326 <vignesh.r@exchange-data.in>
2025-01-03 15:13:15 +0530

    Configured tailwind css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/js/App.tsx')
+    @vite('resources/css/app.css')
 </head>
 <body>
 @inertia
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        './resources/**/*.tsx',
     ],
     theme: {
         extend: {
